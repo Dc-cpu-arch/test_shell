4f3f8c94799e161c2a5853298ae81da5e5b3b27c
Nicolas <nico15935746@gmail.com>
2020-11-12 18:24:40 -0500

    Segmentation fault, but why?

--- shell.c
+++ shell.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-#define BUFF 1024
 
 /**
  *
@@ -14,18 +13,19 @@ int main (void)
 	size_t num_of_bytes = 0;
 	char *sstr = NULL;
 
-	printf("$ ");
-	bytes_of_char = getline(&sstr, &num_of_bytes, stdin);
-
-	if (bytes_of_char <= 0)
+	while (strcmp(sstr, "exit") != 0)
 	{
-		puts ("You need to enter something");
-		return (1);
+		printf("$ ");
+		bytes_of_char = getline(&sstr, &num_of_bytes, stdin);
+
+		if (bytes_of_char <= 1)
+		{
+			puts ("You need to enter something");
+			return (1);
+		}
+		else
+			printf("%s", sstr);
+		free (sstr);
 	}
-	else
-		printf("%s", sstr);
-
-	free (sstr);
-
 	return (0);
 }
