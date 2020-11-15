fd3a3e34167aeed9f4a65691fd3821432a216365
Nicolas <nico15935746@gmail.com>
2020-11-15 18:40:34 -0500

    exit fixed

--- shell.c
+++ shell.c
@@ -7,24 +7,37 @@
  *
  */
 
-int main (void)
+int main(void)
 {
 	ssize_t bytes_of_char;
 	size_t num_of_bytes = 0;
-	char *sstr = "open";
+	char *sstr = "entry";
 
-	while (strcmp(sstr, "exit") != 0)
+	while (sstr)
 	{
 		printf("$ ");
 		bytes_of_char = getline(&sstr, &num_of_bytes, stdin);
 
+		if (sstr[bytes_of_char - 1] == '\n')
+			sstr[bytes_of_char - 1] = '\0';
+
+		if (strcmp(sstr, "exit") == 0)
+		{
+			puts("you typed exit!");
+			free(sstr);
+			return (1);
+		}
+
 		if (bytes_of_char <= 1)
 		{
-			puts ("You need to enter something");
+			puts("You need to enter something");
+			free(sstr);
 			return (1);
 		}
 		else
-			printf("%s", sstr);
+		{
+			printf("%s\n", sstr);
+		}
 	}
 	free(sstr);
 	return (0);
