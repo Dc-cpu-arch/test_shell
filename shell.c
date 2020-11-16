5b124d4ba6c2b206596b7e3e51581e5b17dda61f
Nicolas <nico15935746@gmail.com>
2020-11-16 16:03:25 -0500

    Arg var added

--- shell.c
+++ shell.c
@@ -12,10 +12,14 @@ int main(void)
 	ssize_t bytes_of_char;
 	size_t num_of_bytes = 0;
 	char *sstr = "entry";
+	pid_t child;
+	char *arg[1];
+	arg[0] = sstr;
+	arg[1] = NULL;
 
 	while (sstr)
 	{
-		printf("$ ");
+		printf("$ Process PID %d\n", getpid());
 		bytes_of_char = getline(&sstr, &num_of_bytes, stdin);
 
 		if (sstr[bytes_of_char - 1] == '\n')
@@ -26,6 +30,18 @@ int main(void)
 			puts("you typed exit!");
 			free(sstr);
 			return (1);
+		}
+
+		if (strcmp(sstr, "ls") == 0)
+		{
+			child = fork();
+			//ir al directorio /bin/"command"
+			execv("/bin/ls", arg);
+			if (child == 0)
+			{
+				printf("command Child Process # %d\n", getpid());
+				kill (child, SIGKILL);
+			}
 		}
 
 		if (bytes_of_char <= 1)
