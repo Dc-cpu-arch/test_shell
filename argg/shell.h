9c4f1cee29a8934eb25b659c1fe3db52ea9b4450
Nicolas <nico15935746@gmail.com>
2020-11-21 17:08:50 -0500

    include of wait added

--- argg/shell.h
+++ argg/shell.h
@@ -5,9 +5,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 void floop(void);
 char *read_line(void);
 char **split_line(char *line);
+void enviromentShell(char **sstr);
+void execute(char *path, char *args);
+char *_strcat(char *dest, char *src);
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+
 
 #endif /* SHELL_H */
