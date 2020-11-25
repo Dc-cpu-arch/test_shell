e9c894ed8f4d86c2bb01e7a10234c9fbf44ce016
Nicolas <nico15935746@gmail.com>
2020-11-24 20:20:06 -0500

    other way to env

--- argg/back/path.c
+++ argg/back/path.c
@@ -6,26 +6,29 @@
  */
 int path(char *line, char **args)
 {
-	char *copy, *tokens, *tmp, *command;
+	char *copy, *tokens, *tmp, *command, *tmpp;
 	char *path, *address;
 	char tmp2[1] = "/";
-	//int i;
-	char *tokens2;
+	int i = 0;
+	//char *tokens2;
 
 	struct stat st;
 
-	copy = _getpath();
+	copy = _getenv("PATH", environ);
+	printf("COPY: %s\n", copy);
 	tmp = _strdup(line);
 	address = _strdup(line);
 	tokens = strtok(copy, "PATH=");
-	tokens2 = strtok(tokens, ":");
+	printf("ANTES DE QUITAR DOS PUNTOS\n COPY: %s\n TOKENS %s\n", copy, tokens);
+	tmpp = _strdup(tokens);
+	tokens = strtok(tmpp, ":");
+	printf("DESPUES DE QUITAR DOS PUNTOS\n COPY: %s\n TOKENS %s\n\n", tmpp, tokens);
 	command = strcat(tmp2, tmp);
 
-	printf("tokens = %s\n", tokens);
 	//for (i = 0; tokens2 != NULL; i++)
-	for (tokens2 = strtok(tokens, ":"); tokens2; tokens2 = strtok('\0', " "))
+	while (tokens)
 	{
-		path = _strcat(tokens2, command);
+		path = _strcat(tokens, command);
 		printf("address is =%s\n path = %s\n command is %s\n", address, path, command);
 		if (stat(path, &st) == 0)
 		{
@@ -39,6 +42,7 @@ int path(char *line, char **args)
 		//free(tokens2);
 		//tokens = strtok(NULL, ":");
 		free(path);
+		i++;
 	}
 	free(copy);
 	copy = NULL;
@@ -52,15 +56,41 @@ int path(char *line, char **args)
  *
  *
  */
+
 char *_getpath(void)
 {
-    int index = 0;
+	int index = 0;
 
-    if (!environ || !*environ)
-        return (NULL);
+	if (!environ || !*environ)
+		return (NULL);
 
-    while (strncmp(environ[index], "PATH=", 5))
-	    index++;
+	while (strncmp(environ[index], "PATH=", 5))
+		index++;
 
-    return (environ[index]);
+	return (environ[index]);
+}
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+char *_getenv(const char *name, char **env)
+{
+	int i, j, check, l = 0;
+	if (!env)
+		return (NULL);
+	while (name[l] != 0)
+		l++;
+	for (i = 0; env[i] != 0; i++)
+	{
+		for (check = 0, j = 0; j < l && env[i][j] != 0; j++)
+			if (env[i][j] == name[j])
+				check++;
+		if (check == l && env[i][check] == '=')
+			return (env[i]);
+	}
+	return (NULL);
 }
