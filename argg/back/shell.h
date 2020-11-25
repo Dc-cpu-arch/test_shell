7ffa8d12c37b2e6f5341b25dcc3662627c1f4323
Nicolas <nico15935746@gmail.com>
2020-11-24 21:54:11 -0500

    betty checks

--- argg/back/shell.h
+++ argg/back/shell.h
@@ -12,17 +12,19 @@
 typedef struct stat stat_t;
 
 extern char **environ;
-void floop(char * envp[]);
+void floop(char *envp[]);
+char **_strtok(char *s, char *delm);
 char *read_line(void);
+char *_getenv(const char *name, char **env);
 char **split_line(char *line);
 void enviromentShell(char **sstr);
-int execute(char *path, char **args, char * envp[]);
+int execute(char *path, char **args, char *envp[]);
 int cmp(char *line);
 char *_strcat(char *dest, char *src);
 int _strlen(char *s);
 int _strcmp(char *s1, char *s2);
+char *_strdup(char *str);
 char *_getpath(void);
 int path(char *line, char **args);
-char *_strdup(char *str);
 
 #endif /* SHELL_H */
